8d8eb4066957f108d371ee97072acc3ebe6a0f3b
Ronald <87809003+RONALDARIUS@users.noreply.github.com>
2023-04-29 09:50:29 +0700

    ok

--- leodmainh.lua
+++ leodmainh.lua
@@ -2,31 +2,31 @@ local id = game.PlaceId
 
 
 if id == 11448431235 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxrp1st'))() --rp1st
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxrp1st.lua'))() --rp1st
 elseif id == 12241341621 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxrp2nd'))() --rp2nd
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxrp2nd.lua'))() --rp2nd
 elseif id == 5780309044 then 
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxsa'))() --sa
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxsa.lua'))() --sa
 elseif id == 2951213182 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxeptw'))() --eptw
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxeptw.lua'))() --eptw
 elseif id == 3200010305 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepmain'))() --eptb
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepmain.lua'))() --eptb
 elseif id == 2625195454 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepmain'))() --eptd
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepmain.lua'))() --eptd
 elseif id == 2797881676 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxeptf'))() --eptf
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxeptf.lua'))() --eptf
 elseif id == 4768829954 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepmain'))() --eptd
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepmain.lua'))() --eptd
 elseif id == 7799530284 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepmain'))() --eptc
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepmain.lua'))() --eptc
 elseif id == 3590667014 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxeptl'))() --eptl
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxeptl.lua'))() --eptl
 elseif id == 4518266946 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepts'))() --epts
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepts.lua'))() --epts
 elseif id == 4661507759 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepscrs'))() --epscrs
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepscrs.lua'))() --epscrs
 elseif id == 2215221144 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepkh'))() --epkh
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/rbxepkh.lua'))() --epkh
 else
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/leodmain'))() --main
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RONALDARIUS/Main/master/leodmain.lua'))() --main
 end
